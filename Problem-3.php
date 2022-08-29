cfbec8090b608dd424762f26d99cd60494b89237
maki-engineer <ryohei-81@outlook.jp>
2022-08-29 15:11:52 +0900

    update: Problem-3問題解決

---
+++ Problem-3.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 ?
+*/
+
+function pf($n){
+  $result = [];
+  $init   = 2;
+
+  while($n !== 1){
+    $i = $init;
+
+    while($i < 0xFFFFFF){
+      if($n % $i == 0){
+        $result[] = $i;
+        $n /= $i;
+        break;
+      }
+      $i++;
+    }
+    $init = $i;
+  }
+
+  return $result;
+}
+
+$array = pf(600851475143);
+$result = end($array);
+
+print_r($result);  # >> 6857
