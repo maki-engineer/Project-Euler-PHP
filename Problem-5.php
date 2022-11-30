a18454fb0939b38a1797298fc1935162313ad5dc
maki-engineer <ryohei-81@outlook.jp>
2022-11-30 18:57:42 +0900

    update: 軽微な修正

--- Problem-5.php
+++ Problem-5.php
@@ -5,29 +5,21 @@
  * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 */
 
-$result     = 20;
+$result = 20;
 
 while (true) {
   $notSeached = false;
 
   for ($i = 20; $i > 10; $i--) {
     if ($result % $i !== 0) {
-
       $notSeached = true;
       break;
-
     }
   }
 
-  if ($notSeached) {
+  if (!$notSeached) break;
 
-    $result++;
-
-  } else {
-
-    break;
-
-  }
+  $result++;
 }
 
 echo $result;  # >> 232792560
